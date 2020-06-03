4c9c7f1bb33f59fb21a1adcd03cbe3c128788a0b
AbhikBoruah <64722025+AbhikBoruah@users.noreply.github.com>
2020-06-03 23:42:13 +0530

    js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+    $(".rocket").click(function(){
+      $(".rocket").animate({
+        left: '2500px'
+        
+        
+      });
+    });
+  });
+
+
+
+
 document.querySelector(".anthem-img").addEventListener("click",youclick);
 
 function youclick(){
@@ -5,21 +18,3 @@ function youclick(){
     audio.play();
 }
 
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml3 .letter',
-    opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
-  }).add({
-    targets: '.ml3',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
